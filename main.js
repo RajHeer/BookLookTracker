8431e74f76355d6ba461f2a2e62ffdd56fea52e6
Raj Heer <thisisrajmatazz@gmail.com>
2023-02-07 23:25:40 +0000

    Wire up 'Add Book' btn to toggle display of addBookForm.

--- main.js
+++ main.js
@@ -2,4 +2,14 @@
 
 const mainGrid = document.querySelector('.main_cards');
 const addBookBTN = document.querySelector('#addBookBTN');
+const addBookForm = document.querySelector('#addBookForm');
 
+// LISTENERS //
+
+addBookBTN.addEventListener('click', () => {
+    if (addBookForm.style.display === 'none') {
+        addBookForm.style.display = 'block';
+      } else {
+        addBookForm.style.display = 'none';
+      }
+})
